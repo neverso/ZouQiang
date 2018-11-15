8e9a9132de65238ac0404e5b80d42b3a9b192e5d
zou qiang <719160804@qq.com>
2018-11-15 15:14:53 +0800

    反射工具

---
+++ Assets/ZouQiang/Tool/ReflectionMonoBehaviour/ReflectionMonoBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ReflectionMonoBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public List<MonoBehaviour> allMonoBehaviour = new List<MonoBehaviour>();
+
+    void Start()
+    {
+        GetAllMonoBehaviour();
+    }
+
+    public void GetAllMonoBehaviour()
+    {
+        MonoBehaviour[] MonoBehaviours = transform.GetComponents<MonoBehaviour>();
+
+        for (int i = 0; i < MonoBehaviours.Length; i++)
+        {
+            if (MonoBehaviours[i] == this)
+            {
+                continue;
+            }
+
+            allMonoBehaviour.Add(MonoBehaviours[i]);
+        }
+    }
+}
---
+++ Assets/ZouQiangExample/ReflectionMonoBehaviour/AAAAA.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AAAAA : MonoBehaviour
+{
+    private int a;
+    public int A;
+    private string b;
+    public string B;
+    public List<int> D;
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    public void AA()
+    {
+        Debug.Log("AA");
+    }
+    public void AA(string a)
+    {
+        Debug.Log("AA" + a);
+    }
+}
